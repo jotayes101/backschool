c54ba0b85a9c4dd26ce92e9c13755f0e4975487f
Jota Mario Miranda G <jotayes101@gmail.com>
2018-09-26 09:52:36 -0500

    format

--- _helpers/db.js
+++ _helpers/db.js
@@ -1,8 +1,8 @@
-const config = require('config.json');
-const mongoose = require('mongoose');
+const config = require("config.json");
+const mongoose = require("mongoose");
 mongoose.connect(config.connectionString);
 mongoose.Promise = global.Promise;
 
 module.exports = {
-    User: require('../users/user.model')
+  User: require("../users/user.model")
 };
--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-﻿require('rootpath')();
-const express = require('express');
+﻿require("rootpath")();
+const express = require("express");
 const app = express();
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const jwt = require('_helpers/jwt');
-const errorHandler = require('_helpers/error-handler');
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const jwt = require("_helpers/jwt");
+const errorHandler = require("_helpers/error-handler");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -14,13 +14,13 @@ app.use(cors());
 app.use(jwt());
 
 // api routes
-app.use('/users', require('./users/users.controller'));
+app.use("/users", require("./users/users.controller"));
 
 // global error handler
 app.use(errorHandler);
 
 // start server
-const port = process.env.NODE_ENV === 'production' ? 80 : 4000;
-const server = app.listen(port, function () {
-    console.log('Server listening on port ' + port);
+const port = process.env.NODE_ENV === "production" ? 80 : 4000;
+const server = app.listen(port, function() {
+  console.log("Server listening on port " + port);
 });
